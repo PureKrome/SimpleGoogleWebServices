e573e9b97c9be731b41dde3407aef92349f111b1
Pure Krome <pure.krome@world-domination.com.au>
2018-09-19 16:53:36 +1000

    Updated Details (API Method) with more address data.

---
+++ src/SimpleGoogleWebServices/Names.cs
@@ -0,0 +1,8 @@
+namespace WorldDomination.SimpleGoogleWebServices
+{
+    public class Names
+    {
+        public string LongName { get; set; }
+        public string ShortName { get; set; }
+    }
+}
