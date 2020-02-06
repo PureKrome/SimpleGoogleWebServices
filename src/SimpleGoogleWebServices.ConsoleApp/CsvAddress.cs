d717bebb472f21696cbe8285f5c5c41599cc968c
Pure Krome <pure.krome@world-domination.com.au>
2020-02-06 12:18:02 +1100

    Solution :lipstick:

---
+++ src/SimpleGoogleWebServices.ConsoleApp/CsvAddress.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleGoogleWebServices.ConsoleApp
+{
+    public class CsvAddress
+    {
+        public string Id { get; set; }
+        public string Street { get; set; }
+        public string Suburb { get; set; }
+        public string State { get; set; }
+        public string Postcode { get; set; }
+    }
+}
---
+++ src/SimpleGoogleWebServices.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WorldDomination.SimpleGoogleWebServices;
+using WorldDomination.SimpleGoogleWebServices.Geocode;
+
+namespace SimpleGoogleWebServices.ConsoleApp
+{
+    class Program
+    {
+        private const string GoogleApiKey = "AIzaSyAh5QsHUGZKh1BkifV1VwBYT6s7NCfggpg";
+
+
+    }
+}
